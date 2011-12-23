1151bcbe09b1d14ffa33e44fdf74117be3a1477b
dosimetrist <vasnikolaev-msk@yandex.ru>
2011-12-23 22:22:26 +0300

    in progress

---
+++ src/main/java/ru/hh/school/stdlib/Parser.java
@@ -0,0 +1,11 @@
+package ru.hh.school.stdlib;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vasiliy
+ * Date: 19.12.11
+ * Time: 21:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Parser {
+}
